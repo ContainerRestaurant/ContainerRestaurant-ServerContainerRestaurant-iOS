2d4e80e5917e6c5d92714b8d5be76853e553b32f
0ofKim <>
2021-05-07 01:36:58 +0900

    Bottom Sheet 테스트 추가 todo : sheet 최대 됐을때 ViewController 새로 갈기

---
+++ Restaurant/MainTabBar/Test/TestSheet/TestSheetViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TestSheetViewController.swift
+//  Restaurant
+//
+//  Created by 0ofKim on 2021/05/06.
+//
+
+import UIKit
+import FittedSheets
+
+class TestSheetViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Restaurant/MainTabBar/Test/TestViewController.swift
+++ Restaurant/MainTabBar/Test/TestViewController.swift
@@ -6,25 +6,27 @@
 //
 
 import UIKit
+import FittedSheets
 
 class TestViewController: BaseViewController {
     @IBOutlet weak var mapTestButton: UIButton!
     @IBOutlet weak var loginTestButton: UIButton!
     @IBOutlet weak var popUpTestButton: UIButton!
-
+    @IBOutlet weak var sheetTestButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapTestButton.rx.tap
-            .subscribe(onNext: { [self] _ in
-                self.push(viewController: TestMapViewController())
+            .subscribe(onNext: { [weak self] _ in
+                self?.push(viewController: TestMapViewController())
             })
             .disposed(by: disposeBag)
         
         loginTestButton.rx.tap
-            .subscribe(onNext: { [self] _ in
+            .subscribe(onNext: { [weak self] _ in
                 if #available(iOS 13.0, *) {
-                    self.push(viewController: TestLoginViewController())
+                    self?.push(viewController: TestLoginViewController())
                 } else {
                     print("lower version")
                 }
@@ -32,10 +34,30 @@ class TestViewController: BaseViewController {
             .disposed(by: disposeBag)
 
         popUpTestButton.rx.tap
-            .subscribe(onNext: { [self] _ in
+            .subscribe(onNext: { [weak self] _ in
                 let popupVC = TestPopUpViewController()
                 popupVC.modalPresentationStyle = .overFullScreen
-                present(popupVC, animated: false, completion: nil)
+                self?.present(popupVC, animated: false, completion: nil)
+            })
+            .disposed(by: disposeBag)
+        
+        sheetTestButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                let testViewController = TestLoginViewController()
+                let sheetViewController = SheetViewController(controller: testViewController,
+                                                              sizes: [.fixed(540), .fullscreen],
+                                                              options: SheetOptions(
+                                                                useFullScreenMode: true,
+                                                                shrinkPresentingViewController: false))
+                sheetViewController.dismissOnPull = true
+                sheetViewController.didDismiss = { _ in
+                    print("sheetViewController didDismiss")
+                }
+                if sheetViewController.shouldRecognizePanGestureWithUIControls {
+                    print("pan 시작")
+                }
+                
+                self?.present(sheetViewController, animated: true, completion: nil)
             })
             .disposed(by: disposeBag)
     }
