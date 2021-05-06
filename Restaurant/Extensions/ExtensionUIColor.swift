66fedb9b55d5bb5be17d7d6c3f02a6c219d99c72
0ofKim <>
2021-05-07 02:22:50 +0900

    1. 홈 타이틀 섹션 완성 2. UIColor Extension 추가

---
+++ Restaurant/Extensions/ExtensionUIColor.swift
@@ -0,0 +1,117 @@
+//
+//  ExtensionUIColor.swift
+//  Restaurant
+//
+//  Created by 0ofKim on 2021/05/07.
+//
+
+import UIKit
+import Foundation
+
+extension UIColor {
+
+  @nonobjc class var colorGreyGrey01: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorGreyGrey02: UIColor {
+    return UIColor(white: 245.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorGreyGrey05: UIColor {
+    return UIColor(white: 174.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorGreyGrey04: UIColor {
+    return UIColor(white: 206.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorGreyGrey03: UIColor {
+    return UIColor(white: 236.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorGreyGrey08: UIColor {
+    return UIColor(white: 0.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorGreyGrey07: UIColor {
+    return UIColor(white: 36.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorMainGreen01: UIColor {
+    return UIColor(red: 232.0 / 255.0, green: 251.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorMainGreen03: UIColor {
+    return UIColor(red: 24.0 / 255.0, green: 192.0 / 255.0, blue: 141.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorMainGreen02: UIColor {
+    return UIColor(red: 81.0 / 255.0, green: 212.0 / 255.0, blue: 173.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorMainGreen04: UIColor {
+    return UIColor(red: 20.0 / 255.0, green: 175.0 / 255.0, blue: 128.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorSubYellow01: UIColor {
+    return UIColor(red: 1.0, green: 236.0 / 255.0, blue: 164.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorMainGreen05: UIColor {
+    return UIColor(red: 18.0 / 255.0, green: 160.0 / 255.0, blue: 117.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorSubYellow02: UIColor {
+    return UIColor(red: 1.0, green: 222.0 / 255.0, blue: 91.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorSubYellow03: UIColor {
+    return UIColor(red: 1.0, green: 204.0 / 255.0, blue: 5.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorSubYellow04: UIColor {
+    return UIColor(red: 243.0 / 255.0, green: 194.0 / 255.0, blue: 0.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorSubYellow05: UIColor {
+    return UIColor(red: 231.0 / 255.0, green: 184.0 / 255.0, blue: 0.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorPointOrange02: UIColor {
+    return UIColor(red: 244.0 / 255.0, green: 91.0 / 255.0, blue: 44.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorPointOrange01: UIColor {
+    return UIColor(red: 1.0, green: 182.0 / 255.0, blue: 159.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorPointBlue01: UIColor {
+    return UIColor(red: 96.0 / 255.0, green: 165.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorPointOrange03: UIColor {
+    return UIColor(red: 232.0 / 255.0, green: 73.0 / 255.0, blue: 24.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorPointBlue02: UIColor {
+    return UIColor(red: 24.0 / 255.0, green: 119.0 / 255.0, blue: 242.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var colorPointBlue03: UIColor {
+    return UIColor(red: 0.0, green: 100.0 / 255.0, blue: 229.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var guideSpacing: UIColor {
+    return UIColor(red: 0.0, green: 1.0, blue: 1.0, alpha: 0.4)
+  }
+
+  @nonobjc class var guideGuide: UIColor {
+    return UIColor(red: 1.0, green: 0.0, blue: 1.0, alpha: 0.5)
+  }
+
+  @nonobjc class var colorGreyGrey06: UIColor {
+    return UIColor(white: 117.0 / 255.0, alpha: 1.0)
+  }
+
+}
