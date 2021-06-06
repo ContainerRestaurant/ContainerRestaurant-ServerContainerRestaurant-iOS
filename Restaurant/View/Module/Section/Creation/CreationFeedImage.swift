7432a6eb1c5d5e745c010f43d281a95b07019cb2
uykim3 <uykim3@lotte.net>
2021-06-06 23:48:09 +0900

    [피드쓰기] - Module > Section > Creation 폴더 생성 - 난이도 & 사장님

---
+++ Restaurant/View/Module/Section/Creation/CreationFeedImage.swift
@@ -0,0 +1,17 @@
+//
+//  CreationFeedImage.swift
+//  Restaurant
+//
+//  Created by Lotte on 2021/06/05.
+//
+
+import UIKit
+
+class CreationFeedImage: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Restaurant/View/Module/Section/Creation/LevelOfDifficultyAndWelcome.swift
@@ -0,0 +1,17 @@
+//
+//  LevelOfDifficultyAndWelcome.swift
+//  Restaurant
+//
+//  Created by Lotte on 2021/06/05.
+//
+
+import UIKit
+
+class LevelOfDifficultyAndWelcome: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
