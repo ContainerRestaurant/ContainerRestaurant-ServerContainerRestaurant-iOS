8f74cb91e4d52f8e0e55ce95de8d9e448d2f2d79
uykim3 <uykim3@lotte.net>
2021-06-13 23:53:12 +0900

    [Kakao Login] Coordinator & ViewController 생성

---
+++ Restaurant/View/ViewController/PopUp/LoginPopup/LoginPopupCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  LoginPopupCoordinator.swift
+//  Restaurant
+//
+//  Created by Lotte on 2021/06/13.
+//
+
+import Foundation
---
+++ Restaurant/View/ViewController/PopUp/LoginPopup/LoginPopupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginPopupViewController.swift
+//  Restaurant
+//
+//  Created by Lotte on 2021/06/13.
+//
+
+import UIKit
+
+class LoginPopupViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
