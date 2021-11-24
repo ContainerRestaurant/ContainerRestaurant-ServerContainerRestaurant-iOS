a0f1c8cdc5c905a8cd3324ae5ca5581fe4aff80c
uykim3 <uykim3@lotte.net>
2021-11-25 02:49:05 +0900

    레벨업 팝업 뷰 이미지, 멘트 띄워쓰기 수정 & 진동 추가

--- Restaurant/View/ViewController/PopUp/LevelUpPopup/LevelUpPopupViewController.swift
+++ Restaurant/View/ViewController/PopUp/LevelUpPopup/LevelUpPopupViewController.swift
@@ -22,9 +22,10 @@ class LevelUpPopupViewController: BaseViewController, Storyboard {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        imageView.image = Common.getLevelUpImage(levelUp?.from ?? "")
+        imageView.image = Common.getLevelUpImage(levelUp?.to ?? "")
         setDescriptionLabel()
         setLottie()
+        Common.hapticVibration()
     }
 
     private func setDescriptionLabel() {
@@ -43,7 +44,7 @@ class LevelUpPopupViewController: BaseViewController, Storyboard {
             .bold(string: "\(levelUp?.levelFeedCount ?? 0)번째 ", fontColor: .colorGrayGray06, fontSize: 14)
             .regular(string: "피드를 작성하여\n", fontColor: .colorGrayGray06, fontSize: 14)
             .bold(string: levelUp?.from.components(separatedBy: ". ")[1] ?? "", fontColor: .colorGrayGray06, fontSize: 14)
-            .regular(string: "에서", fontColor: .colorGrayGray06, fontSize: 14)
+            .regular(string: "에서 ", fontColor: .colorGrayGray06, fontSize: 14)
             .bold(string: levelUp?.to.components(separatedBy: ". ")[1] ?? "", fontColor: .colorGrayGray06, fontSize: 14)
             .regular(string: "\(toText) 되었어요!", fontColor: .colorGrayGray06, fontSize: 14)
 
